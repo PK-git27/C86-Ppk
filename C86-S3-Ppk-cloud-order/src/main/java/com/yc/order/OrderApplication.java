3a1c39ebc3b74970d14e62284dfcfa31bb67cda0
pk <pk@DESKTOP-V98436E>
2020-08-27 10:54:58 +0800

    订单用户互通初次提交

---
+++ C86-S3-Ppk-cloud-order/src/main/java/com/yc/order/OrderApplication.java
@@ -0,0 +1,24 @@
+package com.yc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class OrderApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+	/**
+	 * 定义 RestTemplate  Bean
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
