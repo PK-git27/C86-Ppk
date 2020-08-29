2abef876cba8594816e185c4226ff830572e1d71
pk <pk@DESKTOP-V98436E>
2020-08-29 21:47:47 +0800

    提交数据连接 + eureka服务器配置

--- C86-S3-Ppk-crbook-eureka/src/main/java/com/yc/eureka/CrEurekaApplication.java
+++ C86-S3-Ppk-crbook-eureka/src/main/java/com/yc/eureka/CrEurekaApplication.java
@@ -2,16 +2,13 @@ package com.yc.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableEurekaClient
-//服务降级注解
-@EnableCircuitBreaker
+@EnableEurekaServer
 @SpringBootApplication
 public class CrEurekaApplication implements WebMvcConfigurer{
 
