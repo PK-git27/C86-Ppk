bd56c0309b4ced9c56309fb8612cba196a438f77
pk <pk@DESKTOP-V98436E>
2020-08-29 20:57:04 +0800

    修复工程确实 resources 节点的问题

---
+++ C86-S3-Ppk-crbook-user/src/main/java/com/yc/user/UserApplication.java
@@ -0,0 +1,31 @@
+package com.yc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableEurekaClient
+//服务降级注解
+@EnableCircuitBreaker
+@SpringBootApplication
+public class UserApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+	/**
+	 * 定义 RestTemplate  Bean
+	 */
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
